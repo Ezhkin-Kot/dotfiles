5a8536a8b0c0518dce4f24ef3752ab3e4c62ead9
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-09-04 17:25:41 +0400

    feat: make echo crossplatform

--- install.sh
+++ install.sh
@@ -5,13 +5,21 @@ color4="\033[1;33m"
 color5="\033[1;35m"
 defaultColor="\033[0m"
 
+cpEcho() { # cross-platform echo
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "$1"
+  else
+    echo -e "$1"
+  fi
+}
+
 printCat() {
   local color="$1"
   local message="${2:-Meow}"
 
-  echo "             ${color}${message}"
-  echo "             ${color}/"
-  echo "         ${color}/ᐠ｡ꞈ｡ᐟ\\ ${defaultColor}"
+  cpEcho "             ${color}${message}"
+  cpEcho "             ${color}/"
+  cpEcho "         ${color}/ᐠ｡ꞈ｡ᐟ\\ ${defaultColor}"
 }
 
 if [ "$EUID" -eq 0 ]; then
@@ -20,17 +28,17 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 echo ""
-echo "Oh, what a boring terminal you have! Don't worry, now we will fix it."
+cpEcho "Oh, what a boring terminal you have! Don't worry, now we will fix it."
 echo ""
-echo "             ${color1}Meow            ${color2}Meow            ${color3}Meow"
-echo "             ${color1}/               ${color2}/               ${color3}/"
-echo "         ${color1}/ᐠ｡ꞈ｡ᐟ\\         ${color2}/ᐠ｡ꞈ｡ᐟ\\         ${color3}/ᐠ｡ꞈ｡ᐟ\\ ${defaultColor}"
-echo "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
+cpEcho "             ${color1}Meow            ${color2}Meow            ${color3}Meow"
+cpEcho "             ${color1}/               ${color2}/               ${color3}/"
+cpEcho "         ${color1}/ᐠ｡ꞈ｡ᐟ\\         ${color2}/ᐠ｡ꞈ｡ᐟ\\         ${color3}/ᐠ｡ꞈ｡ᐟ\\ ${defaultColor}"
+cpEcho "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
 
-echo "${color3} Let's start the configuration? ${defaultColor}"
+cpEcho "${color3} Let's start the configuration? ${defaultColor}"
 read -p "Press [Enter] to continue or [Ctrl+C] to cancel: "
 if [ $? -ne 0 ]; then
-  echo "Configuration canceled."
+  cpEcho "Configuration canceled."
   exit 1
 fi
 
@@ -40,10 +48,10 @@ sudo -v
 # Install Homebrew and add it to the PATH
 printCat "$color3" "Let's install package manager"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> ~/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+cpEcho >> ~/.zprofile
+cpEcho 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
-echo "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
+cpEcho "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
 
 # Install oh-my-zsh
 printCat "$color1" "Now I will install zsh plugin manager"
@@ -53,13 +61,13 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
 git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
-echo "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
+cpEcho "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
 
 # Install packages
 printCat "$color3" "And I will install some useful packages"
 brew install --cask ghostty
 brew install bat eza fd fzf gcc neovim obfs4proxy openssl@3 ripgrep thefuck tldr tmux tor wget zellij zoxide powerlevel10k sl
-echo "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
+cpEcho "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
 
 # Rewrite configs
 printCat "$color1" "I need to change your .zshrc config."
@@ -91,8 +99,8 @@ git config --global push.autoSetupRemote true
 git config --global pull.rebase true
 
 echo ""
-echo "${color2} Git has been configured successfully!"
-echo "Current Git config:"
+cpEcho "${color2} Git has been configured successfully!"
+cpEcho "Current Git config:"
 git --no-pager config --global --list
 
 # Install JetBrains Mono Nerd font
@@ -105,8 +113,8 @@ printCat "$color3" "Do you want to install Neovim config? [y/n]"
 read -r install_neovim
 if [ "$install_neovim" = "y" ]; then
   git clone git@github.com:Ezhkin-Kot/nvim.git ~/.config/nvim
-  echo "${color3} Neovim has been installed successfully!"
-  echo "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
+  cpEcho "${color3} Neovim has been installed successfully!"
+  cpEcho "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
 fi
 
 printCat "$color2" "Congratulations! Now your terminal has become excellent!"
