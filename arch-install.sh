6b1328d8ffde56d42eea537ed2721c45a15b10ad
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-12-14 15:08:41 +0400

    feat: add zip and unzip packages installation

--- arch-install.sh
+++ arch-install.sh
@@ -36,9 +36,9 @@ yay -S --noconfirm zsh-theme-powerlevel10k-git zen-browser-bin
 # Install packages
 
 if [ "$WITH_GUI_INSTALL" = true ]; then
-  sudo pacman -S --noconfirm less kitty ghostty firefox telegram-desktop zathura yazi bat eza fd fzf gcc rustup go fastfetch neovim wireguard-tools ripgrep thefuck tldr tmux tor wget zellij zoxide sl
+  sudo pacman -S --noconfirm zip unzip less kitty ghostty firefox telegram-desktop zathura yazi bat eza fd fzf gcc rustup go fastfetch neovim wireguard-tools ripgrep thefuck tldr tmux tor wget zellij zoxide sl
 else
-  sudo pacman -S --noconfirm less yazi bat eza fd fzf gcc rustup go fastfetch neovim wireguard-tools ripgrep thefuck tldr tmux tor wget zellij zoxide sl
+  sudo pacman -S --noconfirm zip unzip less yazi bat eza fd fzf gcc rustup go fastfetch neovim wireguard-tools ripgrep thefuck tldr tmux tor wget zellij zoxide sl
 fi
 echo -e "   ${color5}–––––––––––––––––––––––––––––––––––––––––––––––––––––– ${defaultColor}"
 
